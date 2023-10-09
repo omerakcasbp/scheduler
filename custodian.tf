debc6904ba887ebfdbd7cfb5b01e567b424bff91
Omer Akca <oakca@schubergphilis.com>
2023-10-09 12:57:20 +0200

    First commit

--- custodian.tf
+++ custodian.tf
@@ -33,18 +33,18 @@ data "archive_file" "custodian_lambda_archive" {
 
 
 module "cloud_custodian_lambda" {
-  source                           = "github.com/terraform-aws-modules/terraform-aws-lambda"
-  function_name                    = "ResourceScheduler"
-  description                      = "Resource Scheduler"
-  handler                          = "custodian_policy.runcustodian_policy.run"
-  runtime                          = "python3.11"
-  create_package                   = false
-  local_existing_package           = data.archive_file.custodian_lambda_archive.output_path
-  kms_key_arn                      = aws_kms_key.custodian_lambda_key.arn
-  lambda_role                      = aws_iam_role.CustodianLambda.arn
-  tags                             = { custodian-info = "mode=periodic:version=0.9.31" }
-  depends_on                       = [data.archive_file.custodian_lambda_archive]
-  timeout                          = 300
+  source                 = "github.com/terraform-aws-modules/terraform-aws-lambda"
+  function_name          = "ResourceScheduler"
+  description            = "Resource Scheduler"
+  handler                = "custodian_policy.runcustodian_policy.run"
+  runtime                = "python3.11"
+  create_package         = false
+  local_existing_package = data.archive_file.custodian_lambda_archive.output_path
+  kms_key_arn            = aws_kms_key.custodian_lambda_key.arn
+  lambda_role            = aws_iam_role.CustodianLambda.arn
+  tags                   = { custodian-info = "mode=periodic:version=0.9.31" }
+  depends_on             = [data.archive_file.custodian_lambda_archive]
+  timeout                = 300
 }
 
 
