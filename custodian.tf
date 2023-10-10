330eac59d275517f11f84b0ae54673dfe393305d
Omer Akca <oakca@schubergphilis.com>
2023-10-10 11:58:51 +0200

    First commit

--- custodian.tf
+++ custodian.tf
@@ -42,7 +42,7 @@ module "cloud_custodian_lambda" {
   local_existing_package = data.archive_file.custodian_lambda_archive.output_path
   kms_key_arn            = aws_kms_key.custodian_lambda_key.arn
   lambda_role            = aws_iam_role.CustodianLambda.arn
-  tags                   = concat({ custodian-info = "mode=periodic:version=0.9.31" }, var.tags)
+  tags                   = merge({ custodian-info = "mode=periodic:version=0.9.31" }, var.tags)
   depends_on             = [data.archive_file.custodian_lambda_archive]
   timeout                = 300
 }
