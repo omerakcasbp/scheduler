7b5d1ab2c9d71f71c1f7cfc33b3a6bce8346d2ae
Omer Akca <oakca@schubergphilis.com>
2023-10-17 14:22:02 +0200

    First commit

--- custodian.tf
+++ custodian.tf
@@ -69,11 +69,12 @@ module "cloud_custodian_lambda" {
   kms_key_arn                = aws_kms_key.custodian_lambda_key.arn
   lambda_role                = aws_iam_role.CustodianLambda.arn
   tags                       = merge({ custodian-info = "mode=periodic:version=0.9.31" }, var.tags)
-  depends_on                 = [data.archive_file.custodian_lambda_archive]
+  depends_on                 = [data.archive_file.custodian_lambda_archive, aws_kms_key.custodian_lambda_key]
   timeout                    = 300
-  cloudwatch_logs_kms_key_id = aws_kms_key.custodian_lambda_key.key_id
+  cloudwatch_logs_kms_key_id = aws_kms_key.custodian_lambda_key.arn
   vpc_subnet_ids             = [for s in module.module_pip_read.vpcs.shared[var.vpc_env].private_subnets : s.id]
   vpc_security_group_ids     = [aws_security_group.rssg.id]
+  de
 }
 
 data "aws_iam_policy_document" "custodian_lambda" {
