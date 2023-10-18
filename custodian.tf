85a1a245f015d94cbed914ed1a63e927c0c5366e
Omer Akca <oakca@schubergphilis.com>
2023-10-18 14:51:43 +0200

    First commit

--- custodian.tf
+++ custodian.tf
@@ -58,22 +58,46 @@ resource "aws_security_group" "rssg" {
 
 }
 
+#module "cloud_custodian_lambda" {
+#  source                     = "github.com/terraform-aws-modules/terraform-aws-lambda"
+#  function_name              = "ResourceScheduler"
+#  description                = "Resource Scheduler"
+#  handler                    = "custodian_policy.runcustodian_policy.run"
+#  runtime                    = "python3.11"
+#  create_package             = false
+#  local_existing_package     = data.archive_file.custodian_lambda_archive.output_path
+#  kms_key_arn                = aws_kms_key.custodian_lambda_key.arn
+#  lambda_role                = aws_iam_role.CustodianLambda.arn
+#  tags                       = merge({ custodian-info = "mode=periodic:version=0.9.31" }, var.tags)
+#  depends_on                 = [data.archive_file.custodian_lambda_archive, aws_kms_key.custodian_lambda_key]
+#  timeout                    = 300
+#  cloudwatch_logs_kms_key_id = aws_kms_key.custodian_lambda_key.arn
+#  vpc_subnet_ids             = [for s in module.module_pip_read.vpcs.shared[var.vpc_env].private_subnets : s.id]
+#  vpc_security_group_ids     = [aws_security_group.rssg.id]
+#}
+
 module "cloud_custodian_lambda" {
-  source                     = "github.com/terraform-aws-modules/terraform-aws-lambda"
-  function_name              = "ResourceScheduler"
-  description                = "Resource Scheduler"
-  handler                    = "custodian_policy.runcustodian_policy.run"
-  runtime                    = "python3.11"
-  create_package             = false
-  local_existing_package     = data.archive_file.custodian_lambda_archive.output_path
-  kms_key_arn                = aws_kms_key.custodian_lambda_key.arn
-  lambda_role                = aws_iam_role.CustodianLambda.arn
-  tags                       = merge({ custodian-info = "mode=periodic:version=0.9.31" }, var.tags)
-  depends_on                 = [data.archive_file.custodian_lambda_archive, aws_kms_key.custodian_lambda_key]
-  timeout                    = 300
-  cloudwatch_logs_kms_key_id = aws_kms_key.custodian_lambda_key.arn
-  vpc_subnet_ids             = [for s in module.module_pip_read.vpcs.shared[var.vpc_env].private_subnets : s.id]
-  vpc_security_group_ids     = [aws_security_group.rssg.id]
+  source      = "github.com/schubergphilis/terraform-aws-mcaf-lambda?ref=v1.1.0"
+  name        = "ResourceScheduler"
+  description = "Lambda for Resource Scheduler"
+  providers   = { aws.lambda = aws }
+  filename    = data.archive_file.custodian_lambda_archive.output_path
+  kms_key_arn = aws_kms_key.custodian_lambda_key.arn
+  policy      = data.aws_iam_policy_document.custodian_lambda_policy.json
+  runtime     = "python3.11"
+  subnet_ids  = [for s in module.module_pip_read.vpcs.shared[var.vpc_env].private_subnets : s.id]
+  tags        = { custodian-info = "mode=periodic:version=0.9.31" }
+  timeout     = 600
+  handler     = "custodian_policy.runcustodian_policy.run"
+  depends_on  = [data.archive_file.custodian_lambda_archive, aws_kms_key.custodian_lambda_key]
+  security_group_egress_rules = [
+    {
+      description = "Security Group rule for Resource Scheduler"
+      cidr_ipv4   = "0.0.0.0/0"
+      from_port   = "0"
+      to_port     = "65535"
+    }
+  ]
 }
 
 
@@ -117,22 +141,6 @@ data "aws_iam_policy_document" "custodian_lambda_policy" {
     ]
   }
 
-  statement {
-    sid       = ""
-    effect    = "Allow"
-    resources = ["*"]
-
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-      "ec2:CreateNetworkInterface",
-      "ec2:DescribeNetworkInterfaces",
-      "ec2:DeleteNetworkInterface",
-      "ec2:AssignPrivateIpAddresses",
-      "ec2:UnassignPrivateIpAddresses",
-    ]
-  }
 
   statement {
     effect    = "Allow"
@@ -204,23 +212,22 @@ data "aws_iam_policy_document" "kms_policy" {
 }
 
 
-resource "aws_cloudwatch_event_rule" "cloud_custodian_lambda_event_rule" {
-  name                = "cloud-custodian-lambda-event-rule"
-  description         = "scheduled every 1 hour"
+resource "aws_cloudwatch_event_rule" "resource_scheduler_lambda_event_rule" {
+  name                = "github-backup-lambda-event-rule"
+  description         = "retry scheduled every 1 hour"
   schedule_expression = "cron(1 * * * ? *)"
-  tags                = var.tags
 }
 
-resource "aws_cloudwatch_event_target" "profile_generator_lambda_target" {
-  arn  = module.cloud_custodian_lambda.lambda_cloudwatch_log_group_arn
-  rule = aws_cloudwatch_event_rule.cloud_custodian_lambda_event_rule.name
+resource "aws_cloudwatch_event_target" "resource_scheduler_lambda_target" {
+  arn  = module.cloud_custodian_lambda.arn
+  rule = aws_cloudwatch_event_rule.resource_scheduler_lambda_event_rule.name
 }
 
-resource "aws_lambda_permission" "allow_cloudwatch_to_call_rw_fallout_retry_step_deletion_lambda" {
-  statement_id   = "AllowExecutionFromCloudWatch"
-  action         = "lambda:InvokeFunction"
-  function_name  = module.cloud_custodian_lambda.lambda_function_name
-  principal      = "events.amazonaws.com"
-  source_arn     = aws_cloudwatch_event_rule.cloud_custodian_lambda_event_rule.arn
-  source_account = data.aws_caller_identity.current.account_id
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_resourcescheduler_lambda" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = module.cloud_custodian_lambda.name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.resource_scheduler_lambda_event_rule.arn
 }
+
