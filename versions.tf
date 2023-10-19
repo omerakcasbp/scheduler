b958d3cc97b7187c1a2fa085c202d843a29c8713
Omer Akca <oakca@schubergphilis.com>
2023-10-19 10:44:23 +0200

    First commit

--- versions.tf
+++ versions.tf
@@ -5,11 +5,14 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 4.9"
+      configuration_aliases = [aws.pip_read]
     }
+
 
     local = {
       version = "~> 2.4.0"
     }
   }
 }
+
 
